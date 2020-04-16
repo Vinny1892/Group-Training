f21d3e3e03717c840381027c7cc839d33a281d58
Vinicius Espindola <vinnyaoe@gmail.com>
2020-04-16 12:32:43 -0400

    Adicionando dot env no controle de versao

--- mongosetup.js
+++ mongosetup.js
@@ -1,12 +1,22 @@
-db.createUser(
-    {
-        user: 'admin',
-        pwd: 'passwordRoot',
-        roles: [
-            {
-                role: "readWrite",
-                db: "laravel-dev"
-            }
-        ]
-    }
-);
+
+db.createUser({
+    user: "app",
+    pwd: "passwordApp",
+    roles: [
+      {
+        role: "readWrite",
+        db: "laravel-dev",
+      },
+    ],
+  });
+  
+  db.createUser({
+    user: "root",
+    pwd: "passwordRoot",
+    roles: [
+      {
+        role: "readWrite",
+        db: "admin",
+      },
+    ],
+  });
