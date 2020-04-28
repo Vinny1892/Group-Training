6667c6d30d6525c770ad5dacc45d97b141014bed
Vinicius Espindola <vinnyaoe@gmail.com>
2020-04-28 19:27:21 -0400

    adicionando dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
 
     public function show(Request $request ){
         $user = Auth::user();
-        return view('dashboard.index' ,   compact('user') );
+        return view('dashboard.index' );
 
     }
 
