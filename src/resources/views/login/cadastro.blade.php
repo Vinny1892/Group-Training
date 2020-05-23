0a50340fc3e4e69c89a29e324b7b5403cb4b7a35
Mateus Ragazzi Balbino <mateusragazzi.b@gmail.com>
2020-05-23 14:54:22 -0400

    Ajustando CSS das telas de login e cadastro

--- src/resources/views/login/cadastro.blade.php
+++ src/resources/views/login/cadastro.blade.php
@@ -2,73 +2,88 @@
 
 @section('content')
 <div class="tela-login">
-  @if ($errors->any())
-          @foreach ($errors->all() as $error)
-            <div class="card red lighten-5">
-            <div class="card-content  red-text"> <p>{{ $error }}</p> </div>
-            </div>
-          @endforeach
-
-@endif
-  <div class="row z-depth-3  login">
-    <h4 class="center-align" >Registrar-se</h4>
-  <form method="POST" action="{{ route('register.storage') }}" class="row">
-      {{ csrf_field() }}
-      <div class="row">
-        <div class="input-field col s12">
-          <i class="material-icons prefix">person</i>
-          <input id="user" name="name" type="text"   value="{{ old('name')  }}" class="validate">
-          <label for="user">Usuário</label>
+  <div class="container">
+    <section class="container white">
+      <h4 class="titulo-login">Registrar-se</h4>
+      @if ($errors->any())
+      @foreach ($errors->all() as $error)
+      <div class="card red lighten-5">
+        <div class="card-content  red-text">
+          <p>{{ $error }}</p>
         </div>
       </div>
-
+      @endforeach
+      @endif
       <div class="row">
+        <div class="col s6">
+          <div class="card google m-lateral-20">
+            <div class="card-content card-action">
+              <a href="{{ route('login.google') }}">
+                <img style="width: 2vw" src="{{ asset("icons/google.svg") }}" alt="login google">
+                Acessar com Google
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="card facebook m-lateral-20">
+            <div class="card-content card-action">
+              <a href="{{ route('login.facebook') }}">
+                <img style="width: 2vw" src="{{ asset("icons/facebook.svg") }}" alt="login facebook">
+                Acessar com Facebook
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form method="POST" action="{{ route('register.storage') }}" class="row">
+        {{ csrf_field() }}
+        <div class="input-field col s12">
+          <i class="material-icons prefix">person</i>
+          <input id="user" name="name" type="text" value="{{ old('name')  }}" class="validate">
+          <label for="user">Usuário</label>
+        </div>
         <div class="input-field col s12">
           <i class="material-icons prefix">email</i>
           <input id="email" value="{{ old('email') }}" name="email" type="email" class="validate">
           <label for="user">Email</label>
         </div>
-      </div>
-
-      <div class="row">
-          <div class="input-field col s12 ">
-            <i class="material-icons prefix">lock</i>
-            <input id="password" name="password" type="password" class="validate">
-            <label class="" for="password">Senha</label>
-      </div>
-          <div class="row">
-              <div class="input-field col s12 ">
-                  <i class="material-icons prefix">lock</i>
-                  <input id="password" name="password_confirmation" type="password" class="validate">
-                  <label class="" for="password">Confirme sua Senha</label>
-              </div>
-
-      <div class="center-align">
-          <button style="background-color: #7a297a" class=" waves-light waves-effect  btn" type="submit" >Cadastrar
-              <i class="material-icons right">send</i>
+        <div class="input-field col s12 ">
+          <i class="material-icons prefix">lock</i>
+          <input id="password" name="password" type="password" class="validate">
+          <label class="" for="password">Senha</label>
+        </div>
+        <div class="input-field col s12 ">
+          <i class="material-icons prefix">lock</i>
+          <input id="password" name="password_confirmation" type="password" class="validate">
+          <label class="" for="password">Confirme sua Senha</label>
+        </div>
+        <div class="center-align">
+          <button class="btn btn-primary" type="submit">Cadastrar
+            <i class="material-icons right">send</i>
           </button>
-          <div class="center-align" style="margin-top: 20px;" id="parent"> <img style="width: 2vw" src="{{ asset('icons/art.svg')  }}"  alt="seletor de cor"> </div>
+          <p>Já tem uma conta? <a href="{{ route('login') }}">Faça o login</a></p>
+          <!-- <div class="center-align" style="margin-top: 20px;" id="parent"> <img style="width: 2vw" src="{{ asset('icons/art.svg')  }}" alt="seletor de cor"> </div> -->
+        </div>
+      </form>
+    </section>
+  </div>
+</div>
 
-      </div>
-  </form>
-  </div>
-  </section>
-  </div>
-  <script src="https://unpkg.com/vanilla-picker@2"></script>
+<script src="https://unpkg.com/vanilla-picker@2"></script>
 <script>
-   let colorBack = localStorage.getItem('color');
- 
-   let login = document.querySelector('.tela-login');
-   let parent = document.querySelector('#parent');
-   if(colorBack != null){
-        login.style.background = colorBack;
+  let colorBack = localStorage.getItem('color')
 
-   } 
-   let picker = new Picker(parent);
-   picker.onChange = function(color) {
-        localStorage.setItem('color' , color.rgbaString);
-        login.style.background = color.rgbaString;
-    };
+  let login = document.querySelector('.tela-login')
+  let parent = document.querySelector('#parent')
+  if (colorBack != null) {
+    login.style.background = colorBack
 
+  }
+  let picker = new Picker(parent);
+  picker.onChange = function(color) {
+    localStorage.setItem('color', color.rgbaString)
+    login.style.background = color.rgbaString
+  }
 </script>
 @endsection
--- src/resources/views/login/login.blade.php
+++ src/resources/views/login/login.blade.php
@@ -1,66 +1,86 @@
 @extends('authentication.app')
 @section('content')
 <div class="tela-login">
-@if ($errors->any())
-  @foreach ($errors->all() as $error)
-    <div class="card red lighten-5">
-      <div class="card-content  red-text"> <p>{{ $error }}</p> </div>
-    </div>
-  @endforeach
-@endif
-<section class="row z-depth-3 login ">
-  <h4 class="center-align" >Login</h4>
-  <form method="post" action="{{ route('login.login') }}" class="row">
-    @csrf
-    <div class="row">
-      <div class="input-field col s12" >
-        <i class="material-icons prefix">person</i>
-        <input id="email" name="email" value="{{ old('email') }}" type="email" class="validate"/>
-        <label for="email">Email</label>
+  <div class="container">
+    <section class="container white">
+      <h4 class="titulo-login">Login</h4>
+      @if ($errors->any())
+      @foreach ($errors->all() as $error)
+      <div class="card red lighten-4 m-lateral-20">
+        <div class="card-content card-action fad">
+          <p>{{ $error }}</p>
+        </div>
       </div>
-    </div>
+      @endforeach
+      @endif
+      <div class="row">
+        <div class="col s6">
+          <div class="card google m-lateral-20">
+            <div class="card-content card-action">
+              <a href="{{ route('login.google') }}">
+                <img style="width: 2vw" src="{{ asset("icons/google.svg") }}" alt="login google">
+                Acessar com Google
+              </a>
+            </div>
+          </div>
 
-    <div class="row" >
-      <div class="input-field col s12">
-
-        <i class="material-icons prefix">lock</i>
-        <input id="password"  name="password"  type="password" class="validate"/>
-        <label class="" for="password">Senha</label>
+        </div>
+        <div class="col s6">
+          <div class="card facebook m-lateral-20">
+            <div class="card-content card-action">
+              <a href="{{ route('login.facebook') }}">
+                <img style="width: 2vw" src="{{ asset("icons/facebook.svg") }}" alt="login facebook">
+                Acessar com Facebook
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <form method="post" action="{{ route('login.login') }}" class="row">
+        @csrf
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">person</i>
+            <input id="email" name="email" value="{{ old('email') }}" type="email" class="validate" />
+            <label for="email">Email</label>
+          </div>
 
-    <div class="center-align">
-      <button style="background-color: #7a297a" class="waves-effect waves-light btn" type="submit" >Login
-        <i class="material-icons right">send</i>
-      </button>
-    </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock</i>
+            <input id="password" name="password" type="password" class="validate" />
+            <label class="" for="password">Senha</label>
+          </div>
+        </div>
 
-    <div class=" center-align">
-      <p>Não tem conta? <a href="{{ route('register') }}">Registre-se</a></p>
-      <a  href="{{ route('login.google') }}"><img  style="width: 2vw" src="{{ asset("icons/google.svg") }}" alt="login google"></a>
-      <a href="{{ route('login.facebook') }}"><img  style="width: 2vw"  src="{{ asset("icons/facebook.svg") }}" alt="login facebook"></a>
-    </div>
-    <div class="center-align" style="margin-top: 20px;" id="parent"> <img style="width: 2vw" src="{{ asset('icons/art.svg')  }}"  alt="seletor de cor"> </div>
+        <div class="center-align">
+          <button class="btn btn-primary" type="submit">Login
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
 
-  </form>
-</section>
-</div>
-<script src="https://unpkg.com/vanilla-picker@2"></script>
-<script>
-   let colorBack = localStorage.getItem('color');
- 
-   let login = document.querySelector('.tela-login');
-   let parent = document.querySelector('#parent');
-   if(colorBack != null){
-        login.style.background = colorBack;
+        <div class=" center-align">
+          <p>Não tem conta? <a href="{{ route('register') }}">Registre-se</a></p>
+        </div>
+        <!-- <div class="center-align" style="margin-top: 20px;" id="parent"> <img style="width: 2vw" src="{{ asset('icons/art.svg')  }}" alt="seletor de cor"> </div> -->
+      </form>
+    </section>
+  </div>
+  <script src="https://unpkg.com/vanilla-picker@2"></script>
+  <script>
+    let colorBack = localStorage.getItem('color');
 
-   } 
-   let picker = new Picker(parent);
-   picker.onChange = function(color) {
-        localStorage.setItem('color' , color.rgbaString);
-        login.style.background = color.rgbaString;
+    let login = document.querySelector('.tela-login');
+    let parent = document.querySelector('#parent');
+    if (colorBack != null) {
+      login.style.background = colorBack;
+
+    }
+    let picker = new Picker(parent);
+    picker.onChange = function(color) {
+      localStorage.setItem('color', color.rgbaString);
+      login.style.background = color.rgbaString;
     };
-
-</script>
+  </script>
+</div>
 
 @endsection
